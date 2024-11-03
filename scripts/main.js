5851c0664da8ac3b071d69c0130deabaef874214
Zacj Mor <zackmortar9@gmail.com>
2024-11-03 16:22:39 +0200

    added filtering and styling changes

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ dateInput.valueAsDate = new Date();
 const createButton = document.getElementById("createbutton");
 const incomeContainer = document.querySelector(".incomecontainer");
 const expenseContainer = document.querySelector(".expensecontainer");
+const budgetelement = document.querySelector(".budget h2");
+budgetelement.innerHTML = 1000;
 class Transaction {
 	constructor(title, amount, earned, time) {
 		this.title = title;
@@ -76,8 +78,7 @@ createButton.addEventListener("click", () => {
 			dateInput.valueAsDate
 		);
 		expenseTracker.addTransaction(transaction);
-		updateIncome();
-		updateExpense();
+		updateUI();
 	} else {
 		[nameInput, amountInput].forEach((input) => {
 			if (!input.value) {
@@ -94,38 +95,97 @@ createButton.addEventListener("click", () => {
 function updateIncome(filter = () => true) {
 	incomeContainer.innerHTML = "";
 	incomeContainer.appendChild(transactiondiv("Title", "Amount", "Date", 1));
-	expenseTracker.getTransactions().filter(filter).forEach((transaction) => {
-		if (transaction.earned === 1) {
-			incomeContainer.appendChild(
-				transactiondiv(
-					transaction.title,
-					transaction.amount,
-					new Date(transaction.time).toLocaleDateString(),
-					null,
-					transaction
-				)
-			);
-		}
-	});
+	expenseTracker
+		.getTransactions()
+		.filter(filter)
+		.forEach((transaction) => {
+			if (transaction.earned === 1) {
+				incomeContainer.appendChild(
+					transactiondiv(
+						transaction.title,
+						transaction.amount,
+						new Date(transaction.time).toLocaleDateString(),
+						null,
+						transaction
+					)
+				);
+			}
+		});
 }
 
 function updateExpense(filter = () => true) {
 	expenseContainer.innerHTML = "";
 	expenseContainer.appendChild(transactiondiv("Title", "Amount", "Date", 1));
-	expenseTracker.getTransactions().filter(filter).forEach((transaction) => {
-		if (transaction.earned === 0) {
-			expenseContainer.appendChild(
-				transactiondiv(
-					transaction.title,
-					transaction.amount,
-					new Date(transaction.time).toLocaleDateString(),
-					null,
-					transaction
-				)
-			);
-		}
-	});
+	expenseTracker
+		.getTransactions()
+		.filter(filter)
+		.forEach((transaction) => {
+			if (transaction.earned === 0) {
+				expenseContainer.appendChild(
+					transactiondiv(
+						transaction.title,
+						transaction.amount,
+						new Date(transaction.time).toLocaleDateString(),
+						null,
+						transaction
+					)
+				);
+			}
+		});
 }
 
-updateIncome();
-updateExpense();
+function updateUI(filter = () => true) {
+	updateIncome(filter);
+	updateExpense(filter);
+	setbudget();
+}
+
+const minAmountInput = document.getElementById("minAmount");
+const maxAmountInput = document.getElementById("maxAmount");
+const dateFilterInput = document.getElementById("dateFilter");
+const notesFilterInput = document.getElementById("notesFilter");
+
+const filterAmountsButton = document.getElementById("filteramountsbtn");
+const filterDatesButton = document.getElementById("filterdatesbtn");
+const filterNotesButton = document.getElementById("filternotesbtn");
+const resetButton = document.getElementById("resetbtn");
+
+filterAmountsButton.onclick = function () {
+	let minamount = minAmountInput.value ? minAmountInput.value : 0;
+	let maxamount = maxAmountInput.value
+		? maxAmountInput.value
+		: Number.MAX_SAFE_INTEGER;
+	updateUI((element) => {
+		return element.amount > minamount && element.amount < maxamount;
+	});
+};
+
+filterDatesButton.onclick = function () {
+	updateUI((element) => {
+		return (
+			new Date(element.time).toDateString() ===
+			new Date(dateFilterInput.valueAsDate).toDateString()
+		);
+	});
+};
+
+filterNotesButton.onclick = function () {
+	updateUI((element) => {
+		return element.title === notesFilterInput.value;
+	});
+};
+
+resetButton.onclick = () => {
+	updateUI();
+};
+
+function setbudget() {
+	let budget = 0;
+	expenseTracker.getTransactions().forEach((element) => {
+		if (element.earned == 1) budget += element.amount;
+		else budget -= element.amount;
+	});
+	budgetelement.innerHTML = "Budget : " + budget;
+}
+
+updateUI();
